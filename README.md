0bdb65adc2249292ca39a5ec54b7e3228ea81382
Daniele Santini <danysan95@gmail.com>
2023-08-26 12:19:03 +0200

    Update stats

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # dbsn-import
 
-Scripts and notebooks for anlysis and preparation of IGM DBSN data ( https://www.igmi.org/it/dbsn-database-di-sintesi-nazionale , now under ODBL 🎉) to OpenStreetMap.
+Scripts and notebooks for anlysis and preparation of IGM DBSN data for integration in OpenStreetMap.
 
+* [OSM Wiki](https://wiki.openstreetmap.org/wiki/Italy/DBSN)
+* [Official website](https://www.igmi.org/it/dbsn-database-di-sintesi-nazionale)
 Main scripts:
-* [download.sh](./download.sh)
-* [filter.sh](./filter.sh)
-* [townhalls_dbsn.ipynb](./notebooks/townhalls_dbsn.ipynb)
+* [download.sh](./download.sh): Download the files for each province
+* [filter.sh](./filter.sh): Filter all the elements of a certain type
+* [townhalls_dbsn.ipynb](./notebooks/townhalls_dbsn.ipynb): find missing townhalls (run only after creating townhalls.geojson with `./filter.sh edifc "\"edifc_uso\" = '0201'" townhalls geojson`; view on https://www.dsantini.it/dbsn/notebooks/townhalls_dbsn.html )
--- filter.sh
+++ filter.sh
@@ -3,31 +3,34 @@ set -e
 
 # To understand and find the filters, check out https://wiki.openstreetmap.org/wiki/Italy/DBSN#Data_model
 # The list of available layers can be obtained by running ogrinfo on the gdb folder for any province
+# Examples of usage:
+# ./filter.sh edifc "\"edifc_uso\" = '0201'" townhalls geojson
+# ./filter.sh edifc "\"edifc_uso\" = '030102'" hospital_buildings geojson
+# ./filter.sh pe_uins "\"pe_uins_ty\" = '0302'" hospitals geojson
 
-GDAL_FILTER="\"edifc_uso\" = '0201'"
-GDAL_LAYER='edifc'
-DEST_DIR_PATH='./.temporary_data/municipi'
-DEST_FILE_PATH='./notebooks/municipi.geojson'
+DEFAULT_GDAL_LAYER='edifc'
+DEFAULT_GDAL_FILTER="\"edifc_uso\" = '0201'"
+DEFAULT_OUT_NAME="townhalls"
+DEFAULT_OUT_EXTENSION='geojson'
 
-# GDAL_FILTER="\"edifc_uso\" = '030102'"
-# GDAL_LAYER='edifc'
-# DEST_DIR_PATH='./.temporary_data/ospedali'
-# DEST_FILE_PATH='./notebooks/ospedali.geojson'
-
-# TODO add other categories
+GDAL_LAYER="${1:-$DEFAULT_GDAL_LAYER}"
+GDAL_FILTER="${2:-$DEFAULT_GDAL_FILTER}"
+OUT_NAME="${3:-$DEFAULT_OUT_NAME}"
+OUT_EXTENSION="${4:-$DEFAULT_OUT_EXTENSION}"
 
 ZIP_DIR_PATH="../zip"
 UNZIPPED_DIR_PATH='../unzipped'
 
 
-mkdir -p "$DEST_DIR_PATH"
+TEMP_DIR_PATH="./.temporary_data/$OUT_NAME"
+mkdir -p "$TEMP_DIR_PATH"
 mkdir -p "$UNZIPPED_DIR_PATH"
 
 for province_zip_path in "$ZIP_DIR_PATH"/*.zip ; do
     base_name="$(basename $province_zip_path)"
     base_name_no_extension="${base_name%.zip}"
 
-    province_file_path="$DEST_DIR_PATH/$base_name_no_extension.geojson"
+    province_file_path="$TEMP_DIR_PATH/$base_name_no_extension.$OUT_EXTENSION"
     if [ -f "$province_file_path" ]; then
         echo "===> $province_zip_path è già stato estratto e filtrato in $province_file_path"
     else
@@ -42,14 +45,15 @@ for province_zip_path in "$ZIP_DIR_PATH"/*.zip ; do
 
         gdb_dir_path="$(find "$unzipped_dir_path" -maxdepth 2 -type d -name '*.gdb')"
         echo "===> Filtraggio di $gdb_dir_path in $province_file_path"
-        ogr2ogr -f 'GeoJSON' -t_srs 'EPSG:4326' -where "$GDAL_FILTER" "$province_file_path" "$gdb_dir_path" "$GDAL_LAYER"
+        ogr2ogr -t_srs 'EPSG:4326' -where "$GDAL_FILTER" "$province_file_path" "$gdb_dir_path" "$GDAL_LAYER"
         echo "===> Filtraggio in $province_file_path completato"
     fi
 done
 
+DEST_FILE_PATH="./notebooks/dbsn_$OUT_NAME.$OUT_EXTENSION"
 rm -f "$DEST_FILE_PATH"
-for province_file_path in "$DEST_DIR_PATH"/*.geojson ; do
+for province_file_path in "$TEMP_DIR_PATH"/*.$OUT_EXTENSION ; do
     echo "===> Unione di $province_file_path in $DEST_FILE_PATH"
-    ogr2ogr -f 'GeoJSON' -append "$DEST_FILE_PATH" "$province_file_path"
+    ogr2ogr -append "$DEST_FILE_PATH" "$province_file_path"
 done
 echo "===> Unione in $DEST_FILE_PATH completata"
