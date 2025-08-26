330af2863eba9bcafbe4e40abcbd0cb2fcb2c8e3
Daniele Santini <danysan95@gmail.com>
2025-08-26 19:29:10 +0200

    Update dbsn.tsv

--- download.sh
+++ download.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e
 
-ZIP_DIR_PATH="../zip"
+ZIP_DIR_PATH="$(dirname "$0")/zip"
 
 
 mkdir -p "$ZIP_DIR_PATH"
 
-while IFS=$'\t' read -r file_name region province igm_url ; do 
-    if [[ -f "$ZIP_DIR_PATH/$file_name" ]]; then
-        echo "===> '$file_name' ('$province','$region') is already downloaded"
+while IFS=$'\t' read -r file_name region province wmit_url igm_url igm_date ; do 
+    file_path="$ZIP_DIR_PATH/$file_name"
+    if [[ -f "$file_path" ]]; then
+        #echo "===> '$file_path' ('$province','$region') is already downloaded"
+        echo "===> $province OK"
     elif [[ "$province" != "Province" && ( -z "$1" || "$province" == "$1" ) ]]; then
-        echo "===> Downloading '$file_name' ('$province','$region') from $igm_url"
-        curl --fail --output "$ZIP_DIR_PATH/$file_name" "$igm_url" && \
-            echo "===> Download of '$file_name' ('$province','$region') completed" || \
-            echo "===> Download of '$file_name' ('$province','$region') failed"
+        echo "===> Downloading '$file_path' ('$province','$region') from $igm_url"
+        curl --fail --output "$file_path" "$igm_url" && \
+            echo "===> Download of '$file_path' ('$province','$region') completed" || \
+            echo "===> Download of '$file_path' ('$province','$region') failed"
     fi
 done < ./dbsn.tsv
 
