95e7ee172e0e9a0f88350b541a32027f917b47d6
Daniele Santini <danysan95@gmail.com>
2025-08-27 22:10:13 +0200

    Aggiornato URL WMit

--- download.sh
+++ download.sh
@@ -12,8 +12,8 @@ while IFS=$'\t' read -r file_name region province wmit_url igm_url igm_date ; do
         #echo "===> '$file_path' ('$province','$region') is already downloaded"
         echo "===> $province OK"
     elif [[ "$province" != "Province" && ( -z "$1" || "$province" == "$1" ) ]]; then
-        echo "===> Downloading '$file_path' ('$province','$region') from $igm_url"
-        curl --fail --output "$file_path" "$igm_url" && \
+        echo "===> Downloading '$file_path' ('$province','$region') from $wmit_url"
+        curl --fail --output "$file_path" "$wmit_url" && \
             echo "===> Download of '$file_path' ('$province','$region') completed" || \
             echo "===> Download of '$file_path' ('$province','$region') failed"
     fi
